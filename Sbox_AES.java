971c67eb4a3f300ddc9bdb05b75452c74163f30e
JAINIK DEVADA <129617335+DEATHKlLL@users.noreply.github.com>
2025-01-28 00:33:52 +0530

    Update Sbox_AES.java
    test1

--- Sbox_AES.java
+++ Sbox_AES.java
@@ -1,4 +1,4 @@
-class Sbox{
+class Sbox{,xmc nkkcxm
     public static void main(String[] args){
         String[] hexa={"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
         operation obj= new operation();
